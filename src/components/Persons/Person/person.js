9c7c406feac9e73f0f078ce9cb3aed5351826628
Muhammed Sahin <muhammed.sahin@acp.at>
2020-07-29 23:35:11 +0200

    add Cockpit component with module.css file

--- src/components/Persons/Person/person.js
+++ src/components/Persons/Person/person.js
@@ -1,14 +1,14 @@
 import React from "react";
 import classses from "./person.module.css";
 
-const person = (probs) => {
+const person = (props) => {
   return (
     <div className={classses.Person}>
-      <p onClick={probs.click}>
-        I'm {probs.name} and I am {probs.age} years old!
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
       </p>
-      <p>{probs.children}</p>
-      <input type="text" onChange={probs.changed} value={probs.name} />
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
 import classes from "./App.module.css";
-import Person from "../components/Persons/Person/person";
-
-// const StyledButton = styled.button`
-//   background-color: ${(props) => (props.alt ? "red" : "green")};
-//   color: white;
-//   font: inherit;
-//   border: 1px solid blue;
-//   padding: 8px;
-//   cursor: pointer;
-
-//   &:hover {
-//     background-color: ${(props) => (props.alt ? "salmon" : "lightgreen")};
-//     color: black;
-//   }
-// `;
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
   state = {
     persons: [
       { id: "a212", name: "Max", age: 28 },
@@ -26,6 +18,11 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex((p) => {
@@ -56,47 +53,27 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = "";
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
-        </div>
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
       );
-
-      btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(" ")}>This is really working!</p>
-        <button className={btnClass} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
